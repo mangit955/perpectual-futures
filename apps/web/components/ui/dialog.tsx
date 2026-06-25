10f8bb9f2d850ce4378f8c25e3c26292c81c3f5f
Manas Raghuwanshi <manasr955@gmail.com>
2026-06-25 22:36:50 +0530

    type error

--- apps/web/components/ui/dialog.tsx
+++ apps/web/components/ui/dialog.tsx
@@ -11,7 +11,7 @@ function Dialog({ ...props }: DialogPrimitive.Root.Props) {
   return <DialogPrimitive.Root data-slot="dialog" {...props} />
 }
 
-function DialogTrigger({ ...props }: DialogPrimitive.Trigger.Props) {
+function DialogTrigger({ ...props }: Omit<DialogPrimitive.Trigger.Props, 'key'>) {
   return <DialogPrimitive.Trigger data-slot="dialog-trigger" {...props} />
 }
 
